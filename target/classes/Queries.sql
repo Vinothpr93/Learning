0852b7feba584f36a2c1e1def90b8a2fdfd3bcb1
Vinoth Padmanaban <vinothpr980@gmail.com>
2019-08-29 10:37:24 +0530

    Initial commit

---
+++ target/classes/Queries.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS tutorialDb;
+
+DROP TABLE IF EXISTS student;
+
+CREATE TABLE IF NOT EXISTS student (
+  student_id int(100) NOT NULL AUTO_INCREMENT,
+  student_name varchar(50) DEFAULT NULL,
+  roll_number varchar(50) DEFAULT NULL,
+  course varchar(50) DEFAULT NULL,  
+  PRIMARY KEY (student_id)
+);
